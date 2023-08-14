52ebe25607d69937f3dc520dcdd71ca748965dda
PedroCezar2002 <117026622+PedroCezar2002@users.noreply.github.com>
2023-08-14 15:35:06 -0300

    Delete URL.py

--- URL.py
+++
@@ -1,8 +0,0 @@
-url = 'http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100'
-print(url)
-acha_interrogacao = url.find('?')
-url_base = url[:acha_interrogacao]
-print(url_base)
-
-url_parametros = url[acha_interrogacao+1:]
-print(url_parametros)
